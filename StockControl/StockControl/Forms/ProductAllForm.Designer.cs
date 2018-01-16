5137556b8d17fa7daa5b56739cbdfc42f69cfcb1
Aluno <Aluno@DESKTOP-5FEKJGR>
2018-01-16 19:02:38 -0200

    Crie formulário Product All

---
+++ StockControl/StockControl/Forms/ProductAllForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace StockControl.Forms
+{
+    partial class ProductAllForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProductAllForm));
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pbxSearch = new System.Windows.Forms.PictureBox();
+            this.dtgProduct = new System.Windows.Forms.DataGridView();
+            this.pbxBack = new System.Windows.Forms.PictureBox();
+            this.pbxAdd = new System.Windows.Forms.PictureBox();
+            this.pbxEdit = new System.Windows.Forms.PictureBox();
+            this.pbxDelete = new System.Windows.Forms.PictureBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSearch)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgProduct)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxAdd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxEdit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDelete)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(88, 12);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(146, 20);
+            this.tbxName.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // pbxSearch
+            // 
+            this.pbxSearch.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbxSearch.BackgroundImage")));
+            this.pbxSearch.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbxSearch.Location = new System.Drawing.Point(250, 12);
+            this.pbxSearch.Name = "pbxSearch";
+            this.pbxSearch.Size = new System.Drawing.Size(22, 20);
+            this.pbxSearch.TabIndex = 2;
+            this.pbxSearch.TabStop = false;
+            // 
+            // dtgProduct
+            // 
+            this.dtgProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgProduct.Location = new System.Drawing.Point(12, 47);
+            this.dtgProduct.Name = "dtgProduct";
+            this.dtgProduct.Size = new System.Drawing.Size(260, 156);
+            this.dtgProduct.TabIndex = 3;
+            // 
+            // pbxBack
+            // 
+            this.pbxBack.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbxBack.BackgroundImage")));
+            this.pbxBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbxBack.Location = new System.Drawing.Point(12, 221);
+            this.pbxBack.Name = "pbxBack";
+            this.pbxBack.Size = new System.Drawing.Size(31, 28);
+            this.pbxBack.TabIndex = 4;
+            this.pbxBack.TabStop = false;
+            // 
+            // pbxAdd
+            // 
+            this.pbxAdd.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbxAdd.BackgroundImage")));
+            this.pbxAdd.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbxAdd.Location = new System.Drawing.Point(144, 221);
+            this.pbxAdd.Name = "pbxAdd";
+            this.pbxAdd.Size = new System.Drawing.Size(31, 28);
+            this.pbxAdd.TabIndex = 5;
+            this.pbxAdd.TabStop = false;
+            // 
+            // pbxEdit
+            // 
+            this.pbxEdit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbxEdit.BackgroundImage")));
+            this.pbxEdit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbxEdit.Location = new System.Drawing.Point(194, 221);
+            this.pbxEdit.Name = "pbxEdit";
+            this.pbxEdit.Size = new System.Drawing.Size(31, 28);
+            this.pbxEdit.TabIndex = 6;
+            this.pbxEdit.TabStop = false;
+            // 
+            // pbxDelete
+            // 
+            this.pbxDelete.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbxDelete.BackgroundImage")));
+            this.pbxDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbxDelete.Location = new System.Drawing.Point(241, 221);
+            this.pbxDelete.Name = "pbxDelete";
+            this.pbxDelete.Size = new System.Drawing.Size(31, 28);
+            this.pbxDelete.TabIndex = 7;
+            this.pbxDelete.TabStop = false;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(12, 15);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(59, 13);
+            this.lblSearch.TabIndex = 8;
+            this.lblSearch.Text = "Pesquisar :";
+            // 
+            // ProductAllForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.pbxDelete);
+            this.Controls.Add(this.pbxEdit);
+            this.Controls.Add(this.pbxAdd);
+            this.Controls.Add(this.pbxBack);
+            this.Controls.Add(this.dtgProduct);
+            this.Controls.Add(this.pbxSearch);
+            this.Controls.Add(this.tbxName);
+            this.Name = "ProductAllForm";
+            this.Text = "Produto";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSearch)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgProduct)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxAdd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxEdit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDelete)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.PictureBox pbxSearch;
+        private System.Windows.Forms.DataGridView dtgProduct;
+        private System.Windows.Forms.PictureBox pbxBack;
+        private System.Windows.Forms.PictureBox pbxAdd;
+        private System.Windows.Forms.PictureBox pbxEdit;
+        private System.Windows.Forms.PictureBox pbxDelete;
+        private System.Windows.Forms.Label lblSearch;
+    }
+}
