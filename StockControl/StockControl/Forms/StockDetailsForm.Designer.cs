627fcbf18f306fdc2276b8974527a557e0309832
Aluno <Aluno@DESKTOP-5FEKJGR>
2018-01-30 18:10:52 -0200

    criado form estoque

---
+++ StockControl/StockControl/Forms/StockDetailsForm.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace StockControl.Forms
+{
+    partial class StockDetailsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbxSave = new System.Windows.Forms.PictureBox();
+            this.pbxDelete = new System.Windows.Forms.PictureBox();
+            this.pbxBack = new System.Windows.Forms.PictureBox();
+            this.cbxActive = new System.Windows.Forms.CheckBox();
+            this.tbxAmount = new System.Windows.Forms.TextBox();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.lblActive = new System.Windows.Forms.Label();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSave)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDelete)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbxSave
+            // 
+            this.pbxSave.BackgroundImage = global::StockControl.Properties.Resources.salvar;
+            this.pbxSave.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbxSave.Location = new System.Drawing.Point(125, 195);
+            this.pbxSave.Name = "pbxSave";
+            this.pbxSave.Size = new System.Drawing.Size(32, 29);
+            this.pbxSave.TabIndex = 17;
+            this.pbxSave.TabStop = false;
+            this.pbxSave.Click += new System.EventHandler(this.pbxSave_Click);
+            // 
+            // pbxDelete
+            // 
+            this.pbxDelete.BackgroundImage = global::StockControl.Properties.Resources.delete;
+            this.pbxDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbxDelete.Location = new System.Drawing.Point(173, 195);
+            this.pbxDelete.Name = "pbxDelete";
+            this.pbxDelete.Size = new System.Drawing.Size(32, 29);
+            this.pbxDelete.TabIndex = 16;
+            this.pbxDelete.TabStop = false;
+            // 
+            // pbxBack
+            // 
+            this.pbxBack.BackgroundImage = global::StockControl.Properties.Resources.voltar;
+            this.pbxBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbxBack.Location = new System.Drawing.Point(12, 195);
+            this.pbxBack.Name = "pbxBack";
+            this.pbxBack.Size = new System.Drawing.Size(32, 29);
+            this.pbxBack.TabIndex = 15;
+            this.pbxBack.TabStop = false;
+            this.pbxBack.Click += new System.EventHandler(this.pbxBack_Click);
+            // 
+            // cbxActive
+            // 
+            this.cbxActive.AutoSize = true;
+            this.cbxActive.Location = new System.Drawing.Point(65, 129);
+            this.cbxActive.Name = "cbxActive";
+            this.cbxActive.Size = new System.Drawing.Size(15, 14);
+            this.cbxActive.TabIndex = 14;
+            this.cbxActive.UseVisualStyleBackColor = true;
+            // 
+            // tbxAmount
+            // 
+            this.tbxAmount.Location = new System.Drawing.Point(86, 82);
+            this.tbxAmount.Name = "tbxAmount";
+            this.tbxAmount.Size = new System.Drawing.Size(116, 20);
+            this.tbxAmount.TabIndex = 13;
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(59, 36);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(143, 20);
+            this.tbxName.TabIndex = 12;
+            // 
+            // lblActive
+            // 
+            this.lblActive.AutoSize = true;
+            this.lblActive.Location = new System.Drawing.Point(16, 129);
+            this.lblActive.Name = "lblActive";
+            this.lblActive.Size = new System.Drawing.Size(37, 13);
+            this.lblActive.TabIndex = 11;
+            this.lblActive.Text = "Ativo :";
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Location = new System.Drawing.Point(12, 82);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(68, 13);
+            this.lblAmount.TabIndex = 10;
+            this.lblAmount.Text = "Quantidade :";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 36);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(41, 13);
+            this.lblName.TabIndex = 9;
+            this.lblName.Text = "Nome :";
+            // 
+            // StockDetailsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(217, 242);
+            this.Controls.Add(this.pbxSave);
+            this.Controls.Add(this.pbxDelete);
+            this.Controls.Add(this.pbxBack);
+            this.Controls.Add(this.cbxActive);
+            this.Controls.Add(this.tbxAmount);
+            this.Controls.Add(this.tbxName);
+            this.Controls.Add(this.lblActive);
+            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.lblName);
+            this.Name = "StockDetailsForm";
+            this.Text = "Estoque";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSave)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDelete)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBack)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxSave;
+        private System.Windows.Forms.PictureBox pbxDelete;
+        private System.Windows.Forms.PictureBox pbxBack;
+        private System.Windows.Forms.CheckBox cbxActive;
+        private System.Windows.Forms.TextBox tbxAmount;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.Label lblActive;
+        private System.Windows.Forms.Label lblAmount;
+        private System.Windows.Forms.Label lblName;
+    }
+}
