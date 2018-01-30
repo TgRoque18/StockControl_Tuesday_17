a1dc34837899e8b70192c7fbbf7f8c5fa038fdf0
Aluno <Aluno@DESKTOP-5FEKJGR>
2018-01-30 19:04:52 -0200

    insert stock

--- StockControl/StockControl/Forms/StockDetailsForm.cs
+++ StockControl/StockControl/Forms/StockDetailsForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,11 @@ namespace StockControl.Forms
 {
     public partial class StockDetailsForm : Form
     {
+        string name;
+        int quantity = 0;
+        bool active;
+        string connectionString = "workstation id=StockControl.mssql.somee.com;packet size = 4096; user id = luacademy_SQLLogin_1; pwd=msctq6gvt3;data source = StockControl.mssql.somee.com; persist security info=False;initial catalog = StockControl";
+
         public StockDetailsForm()
         {
             InitializeComponent();
@@ -27,6 +33,54 @@ namespace StockControl.Forms
         private void pbxSave_Click(object sender, EventArgs e)
         {
 
+            GetData();
+
+            SqlConnection sqlConnect = new SqlConnection(connectionString);
+
+            try
+            {
+                //Conectar
+                sqlConnect.Open();
+                string sql = "INSERT INTO STOCK(QUANTITY, ACTIVE, NAME) VALUES (@quantity, @active, @name)";
+                //string sql = "INSERT INTO CATEGORY(NAME, ACTIVE) VALUES (" 
+                //    + this.tbxName.Text + "," + this.cbxActive.Checked + ")";
+
+                SqlCommand cmd = new SqlCommand(sql, sqlConnect);
+
+                cmd.Parameters.Add(new SqlParameter("@quantity", quantity));
+                cmd.Parameters.Add(new SqlParameter("@active", active));
+                cmd.Parameters.Add(new SqlParameter("@name", name));
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Adicionado com sucesso!");
+                CleanData();
+                
+            }
+            catch (Exception ex)
+            {
+                //Tratar exce��es
+                MessageBox.Show("Erro ao adicionar categoria!" + ex.Message);
+                CleanData();
+            }
+            finally
+            {
+                //Fechar
+                sqlConnect.Close();
+
+            }
+        }
+        void GetData()
+        {
+            name = tbxName.Text;
+            quantity = Int32.Parse(tbxAmount.Text);
+            active = cbxActive.Checked;
+        }
+        void CleanData()
+        {
+            tbxName.Text = "";
+            tbxAmount.Text = "";
+            cbxActive.Checked = false;
         }
     }
 }
