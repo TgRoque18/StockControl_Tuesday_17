8cf85a244d839fe4dd83b05d99177498b79ba2fd
Aluno <Aluno@DESKTOP-5FEKJGR>
2018-01-30 19:04:55 -0200

    insert no user

--- StockControl/StockControl/Forms/UserDetailsForm.cs
+++ StockControl/StockControl/Forms/UserDetailsForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,15 @@ namespace StockControl.Forms
 {
     public partial class UserDetailsForm : Form
     {
+        string name;
+        string email;
+        string password;
+        string confirm;
+        string profile;
+        bool active;
+
+        string connectionString = "workstation id=StockControl.mssql.somee.com;packet size = 4096; user id = luacademy_SQLLogin_1; pwd=msctq6gvt3;data source = StockControl.mssql.somee.com; persist security info=False;initial catalog = StockControl";
+
         public UserDetailsForm()
         {
             InitializeComponent();
@@ -24,6 +34,67 @@ namespace StockControl.Forms
             UserAllForms userAllForms = new UserAllForms();
             userAllForms.Show();
             this.Hide();
+        }
+
+        private void pbxSave_Click(object sender, EventArgs e)
+        {
+            GetData();
+
+            SqlConnection sqlConnect = new SqlConnection(connectionString);
+
+            try
+            {
+                //Conectar
+                sqlConnect.Open();
+                string sql = "INSERT INTO USER(NAME, PASSWORD, EMAIL, ACTIVE) VALUES (@name, @password, @email, @active)";
+                //string sql = "INSERT INTO CATEGORY(NAME, ACTIVE) VALUES (" 
+                //    + this.tbxName.Text + "," + this.cbxActive.Checked + ")";
+
+                SqlCommand cmd = new SqlCommand(sql, sqlConnect);
+
+                cmd.Parameters.Add(new SqlParameter("@name", name));
+                cmd.Parameters.Add(new SqlParameter("@password", password));
+                cmd.Parameters.Add(new SqlParameter("@email", email));
+                cmd.Parameters.Add(new SqlParameter("@active", active));
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Adicionado com sucesso!");
+                CleanData();
+              
+            }
+            catch (Exception ex)
+            {
+                //Tratar exce��es
+                MessageBox.Show("Erro ao adicionar categoria!" + ex.Message);
+                CleanData();
+            }
+            finally
+            {
+                //Fechar
+                sqlConnect.Close();
+
+            }
+
+        }
+        void GetData()
+        {
+            name = tbxName.Text;
+            email = tbxEmail.Text;
+            password = tbxPassword.Text;
+            confirm = tbxConfirm.Text;
+            profile = tbxProfile.Text;
+            active = cbxActive.Checked;
+        }
+        void CleanData()
+        {
+            tbxName.Text = "";
+            tbxEmail.Text = "";
+            tbxPassword.Text = "";
+            tbxConfirm.Text = "";
+            tbxProfile.Text = "";
+            cbxActive.Checked = false;
+                 
         }
     }
 }
